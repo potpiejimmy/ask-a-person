fd6273fe2b231a177ad65dafaadfd2f728f8b837
potpiejimmy <potpiejimmy@gmail.com>
2024-11-17 22:05:07 +0100

    minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
 
   const [question, setQuestion] = React.useState<string>("");
+  const [loading, setLoading] = React.useState<boolean>(false);
   const [responseTitle, setResponseTitle] = React.useState<string>("");
   const [responseContent, setResponseContent] = React.useState<string>("");
 
@@ -12,12 +13,14 @@ function App() {
 
   async function onkeyup(event: React.KeyboardEvent<HTMLInputElement>) {
     if (event.key === 'Enter') {
-      console.log("Enter");
+      setLoading(true);
       setResponseTitle(question);
+      setResponseContent("...");
       setQuestion("");
 
       let response = await api.ask(question);
       setResponseContent(response.answer);
+      setLoading(false);
     }
   }
 
@@ -27,14 +30,14 @@ function App() {
         Frag <a href="https://www.gruene.de/ueber-uns/robert-habeck">Robert Habeck</a>:
       </div>
       <div>
-        <input autoFocus name="Question" id="question" value={question} className="input" type="email" size={60} onKeyUp={onkeyup} onChange={e=>setQuestion(e.target.value)}/>
+        <input readOnly={loading} autoFocus name="Question" id="question" value={question} className="input" type="email" size={60} onKeyUp={onkeyup} onChange={e=>setQuestion(e.target.value)}/>
       </div>
-      {responseTitle.length > 0 && <div>Frage:</div>}
+      {responseTitle.length > 0 && <div className="font-bold">Frage:</div>}
       <div>
         {responseTitle}
       </div>
-      {responseContent.length > 0 && <div>Antwort:</div>}
-      <div>
+      {responseContent.length > 0 && <div className="font-bold">Antwort:</div>}
+      <div className="whitespace-pre-line">
         {responseContent}
       </div>
     </div>
