9818720778b634f90ce46e233ff8f53ad6b53967
potpiejimmy <t.liese@outlook.com>
2024-11-19 13:18:53 +0100

    intelligent switching, ui enhancements

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,51 @@ import { Checkbox } from './components/Checkbox';
 import { GoInfo } from "react-icons/go";
 import { FaRegCheckCircle } from "react-icons/fa";
 import { BiNoEntry } from "react-icons/bi";
+import { IoHourglassOutline } from "react-icons/io5";
+
+interface PersonContext {
+  checked: boolean;
+  loading: boolean;
+  response: string;
+}
 
 function App() {
 
+  const personContextDefault: PersonContext = {
+    checked: false,
+    loading: false,
+    response: ""
+  };
+
   const availablePersons: { [key: string]: { name: string; info: string } } = {
-    habeck: {
+    "habeck": {
       name: "Robert Habeck",
       info: "https://www.gruene.de/leute/robert-habeck"
     },
-    weidel: {
+    "weidel": {
       name: "Alice Weidel",
       info: "https://www.afd.de/alice-weidel"
+    },
+    "scholz": {
+      name: "Olaf Scholz",
+      info: "https://olaf-scholz.spd.de/start"
+    },
+    "merz": {
+      name: "Friedrich Merz",
+      info: "https://www.merz.cdu.de/"
     }
   }
 
-  const personSelectionDef: { [key: string]: boolean } = {
-    habeck: false,
-    weidel: false
-  }
+  const personContext: { [key: string]: PersonContext } = {};
+  const setPersonContext: { [key: string]: React.Dispatch<React.SetStateAction<PersonContext>> } = {};
+  [personContext["habeck"], setPersonContext["habeck"]] = React.useState<PersonContext>(personContextDefault);
+  [personContext["weidel"], setPersonContext["weidel"]] = React.useState<PersonContext>(personContextDefault);
+  [personContext["scholz"], setPersonContext["scholz"]] = React.useState<PersonContext>(personContextDefault);
+  [personContext["merz"], setPersonContext["merz"]] = React.useState<PersonContext>(personContextDefault);
 
   const [question, setQuestion] = React.useState<string>("");
   const [loading, setLoading] = React.useState<boolean>(false);
-  const [responseTitle, setResponseTitle] = React.useState<string>("");
-  const [responseContents, setResponseContents] = React.useState<any>({});
-  const [personSelection, setPersonSelection] = React.useState(personSelectionDef);
+  const [acceptedQuestion, setAcceptedQuestion] = React.useState<string>("");
   const [checkResult, setCheckResult] = React.useState<string>("");
 
   let api = new AskAPi();
@@ -36,13 +57,11 @@ function App() {
   async function checkQuestion() {
     setLoading(true);
     setCheckResult("");
-    setResponseTitle("");
-    setResponseContents({});
-    setPersonSelection(personSelectionDef);
     let res = await api.ask("checkbot", question);
     if (res.answer === 'Ja') {
       setQuestion("");
-      setResponseTitle(question);
+      setAcceptedQuestion(question);
+      performQuestionToAll(question);
     }
     setCheckResult(res.answer);
     setLoading(false);
@@ -58,72 +77,72 @@ function App() {
     return checkResult === 'Ja';
   }
 
-  async function performQuestion(person: string) {
+  async function performQuestionToAll(question: string) {
+    Object.keys(personContext).forEach(key => {
+      if (personContext[key].checked) {
+        performQuestion(key, question);
+      }
+    });
+  }
+
+  async function performQuestion(person: string, question: string) {
       setLoading(true);
 
-      setResponseContents({
-        ...responseContents,
-        [person]: {answer: "..."}
-      });
+      setPersonContext[person]({checked: true, loading: true, response: "..."});
 
-      let response = await api.ask(person, responseTitle)
+      let response = await api.ask(person/*"dummy"*/, question)
 
-      setResponseContents({
-        ...responseContents,
-        [person]: response
-      });
+      setPersonContext[person]({checked: true, loading: false, response: response.answer});
 
       setLoading(false);
   }
 
   async function personSelected(key: string, checked: boolean) {
-    setPersonSelection({...personSelection, [key]: checked});
+    let loading = false;
     if (checked) {
-      performQuestion(key);
+      if (isQuestionValid()) {
+        loading = true;
+        performQuestion(key, acceptedQuestion);
+      } else {
+        setPersonContext[key]({checked: true, loading: false, response: ""});
+      }
     } else {
-      delete responseContents[key];
-      setResponseContents(responseContents);
+      setPersonContext[key](personContextDefault);
     }
   }
 
   return (
-    <div className="mx-10 my-14 flex flex-col gap-3">
-      <div>
-        <p className="input-container">
-            <input readOnly={loading} autoFocus onKeyUp={onkeyup} value={question} onChange={e=>setQuestion(e.target.value)}
-                   type="text" placeholder="Gib hier deine Frage ein" name="text" id="text" className="input-field"/>
-            <label className="input-label">Deine Frage:</label>
-        </p>
-      </div>
-      {checkResult &&
-        <div>
-          Fragen-Check: {isQuestionValid() ? <FaRegCheckCircle size={40}/> : <BiNoEntry size={40}/>}
-        </div>
-      }
-      {responseTitle.length > 0 && <div className="font-bold">Frage:</div>}
-      <div>
-        {responseTitle}
-      </div>
-      {isQuestionValid() &&
-        <div className="flex flex-col gap-3">
-          <div>Stelle diese Frage an</div>
-          {Object.keys(availablePersons).map((key) => (
-              <div key={key}>
-                <div className='flex flex-row items-center'>
-                  <Checkbox id={key} disabled={!isQuestionValid() || loading} label={availablePersons[key].name} checked={personSelection[key]} onChange={checked => personSelected(key, checked)}/>
-                  <a href={availablePersons[key].info}><GoInfo/></a>
-                </div>
-              </div>
-            )
-          )}
-        </div>
-      }
+    <div className="m-10 flex flex-col gap-5">
+      <div>Stelle deine Frage an</div>
       <div className="flex flex-row gap-3">
-        {Object.keys(responseContents).map((key) => (
+        {Object.keys(availablePersons).map((key) => (
+          <div key={key}>
+            <div className='flex flex-row items-center'>
+              <Checkbox id={key} disabled={personContext[key].loading} label={availablePersons[key].name} checked={personContext[key].checked} onChange={checked => personSelected(key, checked)}/>
+              <a href={availablePersons[key].info} target="_blank"><GoInfo/></a>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex flex-row gap-3 items-center">
+        <p className="grow input-container">
+          <input readOnly={loading} autoFocus onKeyUp={onkeyup} value={question} onChange={e=>setQuestion(e.target.value)}
+                 type="text" name="text" id="question" className="input" placeholder="Gib hier deine Frage ein"></input>
+        </p>
+        <div>
+          {loading ? <IoHourglassOutline size={40}/> : (isQuestionValid() ? <FaRegCheckCircle size={40}/> : <BiNoEntry size={40}/>)}
+        </div>
+      </div>
+      {acceptedQuestion.length > 0 && <div className="font-bold">Frage:</div>}
+      <div className='text-2xl'>
+        {acceptedQuestion}
+      </div>
+      <div className="flex flex-row gap-3">
+        { Object.entries(personContext).filter(([k,v])=>v.checked).map(([key,ctx]) => (
           <div key={key} className="grow basis-0 flex flex-col gap-3">
             <div className="font-bold">{availablePersons[key].name}</div>
             <div className="whitespace-pre-line">
-              {responseContents[key].answer}
+              {ctx.response}
             </div>
           </div>
         ))}
